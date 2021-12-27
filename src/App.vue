e9e299b8f6dd66351de1c8aa955a0d2a598f9d37
Tyrunas Jokubauskas <tyrunasj@synergy.lt>
2021-12-27 12:40:54 +0200

    darkMode

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,28 @@
 <script setup>
+// libraries
+import { useQuasar } from "quasar";
+import { ref, watch } from "vue";
+// components
 import WeekList from "./components/WeekList.vue";
 import MonthList from "./components/MonthList.vue";
 import { useSockets } from "./hooks/useSockets";
 import DayList from "./components/DayList.vue";
 import ActivityTracker from "./components/ActivityTracker.vue";
 const { authorized } = useSockets();
+const q = useQuasar();
+const isDark = ref(false);
+
+watch(isDark, val => {
+  q.dark.set(val);
+});
 </script>
 
 <template>
   <div>
+    <div class="row justify-between">
+      <q-toggle v-model="isDark" label="Dark Mode" />
+      <div class="text-weight-thin">v0.0.1</div>
+    </div>
     <div class="row justify-center" v-if="authorized">
       <DayList />
       <WeekList />
